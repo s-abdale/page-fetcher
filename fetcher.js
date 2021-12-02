338da874bf5965bc016b413ac7234465ff653394
Safia Abdale <safia.abdale@gmail.com>
2021-12-02 14:47:52 +0000

    Refactored code to take command line arguments

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,28 @@
-const request = require('request');
-const fs = require('fs')
+const request = require('request'); // for HTTP request
+const fs = require('fs') // for writing to file
+const args = process.argv.slice(2); // takes cmd line arguments
+
+let urlArg = args[0];
+let pathArg = args[1];
 
 
 // HTTP request
-request('http://example.edu', (error, response, body) => {
-  
+request(urlArg, (error, response, body) => {
   // Write to file
-  fs.writeFile('/vagrant/w2/d3/page-fetcher/index.html', body, err => {
+  fs.writeFile(pathArg, body, err => {
     if (err) {
       console.error(err)
       return
     }
-    //file written successfully
-    const {size} = fs.statSync('/vagrant/w2/d3/page-fetcher/index.html');
+    // Confirmation message
+    const {size} = fs.statSync(pathArg);
     console.log(`Downloaded and saved ${size} bytes to ./index.html`)
   })
 });
+ 
 
+/*
+// Testing: input the following url and path in the command line
+  url = http://example.edu
+  path = /vagrant/w2/d3/page-fetcher/index.html
+*/
